67decf66164e93b10891aefae564143ea3327652
Andrew Popko <popkoandr@gmail.com>
2018-09-06 18:34:49 +0300

    Init wrapper for canvas

---
+++ WpfWorkplace/WpfWorkplace/CustomControls/Wrappers/DrawingCanvas.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace WpfWorkplace.CustomControls.Wrappers
+{
+    public class DrawingCanvas: Canvas
+    {
+        private List<Visual> visuals = new List<Visual>();
+
+        public DrawingCanvas()
+        {
+
+        }
+        
+
+        protected override int VisualChildrenCount => visuals.Count;
+        protected override Visual GetVisualChild(int index)
+        {
+            return visuals[index];
+        }
+
+        public void AddVisual(Visual visual)
+        {
+            visuals.Add(visual);
+
+            base.AddVisualChild(visual);
+            base.AddLogicalChild(visual);
+        }
+
+        public void DeleteVisual(Visual visual)
+        {
+            visuals.Remove(visual);
+
+            base.RemoveVisualChild(visual);
+            base.RemoveLogicalChild(visual);
+        }
+
+        private void drawingSurface_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Hello!");
+        }
+    }
+}
--- WpfWorkplace/WpfWorkplace/Views/SquaresField.xaml.cs
+++ WpfWorkplace/WpfWorkplace/Views/SquaresField.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfWorkplace
+namespace WpfWorkplace.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for SquaresField.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class SquaresField : Window
     {
-        public MainWindow()
+        public SquaresField()
         {
             InitializeComponent();
-        }
-        
-        private void drawingSurface_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            MessageBox.Show("Hello2!");
         }
     }
 }
