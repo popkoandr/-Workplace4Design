19fd8c651b62135b818f93ec00e770a205cd91ce
Andrew Popko <popkoandr@gmail.com>
2018-11-08 17:50:01 +0100

    Func,Actions. https://www.youtube.com/watch?v=e2H15MCOvm8

--- WpfWorkplace/Workplace.Console/DelegatesEvents/MathService.cs
+++ WpfWorkplace/Workplace.Console/DelegatesEvents/MathService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace Workplace.Console.DelegatesEvents
 {
@@ -14,12 +15,14 @@ namespace Workplace.Console.DelegatesEvents
         // //public OutBoundHandler OutboundDelegate; after we've create an event we dont need this anymore.
         //public event OutBoundHandler OutboundEvent;
 
-        public event EventHandler<MathPerformedEventArgs> MathPerformed;
+        public event EventHandler<MathPerformedEventArgs> MathPerformed1;
 
         public double AddNumbers(double value1, double value2)
         {
             var res = value1 + value2;
-            MathPerformed(this, new MathPerformedEventArgs(res));
+            MathPerformed1(this, new MathPerformedEventArgs(res));
+            
+            MathPerformed2(res);
             return res;
         }
 
@@ -27,5 +30,42 @@ namespace Workplace.Console.DelegatesEvents
         {
             return value1 * value2;
         }
+
+        //========== 2
+        public delegate void MathPerformedHandler(double result);
+        public event MathPerformedHandler MathPerformed2;
+
+        //===========2.1
+        public delegate double CalculatationHandler(double val1, double val2);       
+
+        public void CalculateNumbers2(double val1, double val2, CalculatationHandler calculation)
+        {
+            //var res = val1 + val2;
+            //MathPerformed1(this, new MathPerformedEventArgs(res));
+            //MathPerformed2(res);
+            Timer timer = new Timer(5000);
+            MathPerformed2(calculation(val1, val2));  
+        }
+
+        ////=========2.2
+        //public delegate void MathPerformedHandler(double result);
+        //public event MathPerformedHandler MathPerformed2;
+        // we can simply change this on 
+        public Action<double> MathPerformed3;
+
+
+        //public delegate double CalculatationHandler(double val1, double val2);
+        // we can simply change this on 
+        public Func<double, double, double> CalculatationHandler3;
+
+        // 
+        public void CalculateNumbers3(double v1, double v2, Func<double,double, double> calculation)
+        {
+            MathPerformed3(calculation(v1, v2));
+        }
+
+
+
+
     }
 }
--- WpfWorkplace/Workplace.Console/Program.cs
+++ WpfWorkplace/Workplace.Console/Program.cs
@@ -13,9 +13,22 @@ namespace Workplace.Console
         {
             MathService service = new MathService();
             new List<IMathPerformService> { new LogginService(), new NotificationService()
-            }.ForEach(serv => service.MathPerformed += serv.OnMathPerformed);
-           // service.MathPerformed += OnOutBountEvent;
+            }.ForEach(serv => service.MathPerformed1 += serv.OnMathPerformed);
+            // service.MathPerformed += OnOutBountEvent;
+            service.MathPerformed1 += (sender, e) => //is the same as = delegate(object sender, MathPerformedEventArgs args){ 
+            {
+                System.Console.WriteLine($"Calculation RESULT {e.MathResult}");
+            };
             service.AddNumbers(4, 5);
+
+
+            service.MathPerformed2 += (res) =>
+             {
+                 System.Console.WriteLine($"Calculation RESULT {res}");
+             };
+
+            service.CalculateNumbers2(3, 3, (val1, val2) => val1 + val2); //(val1, val2) => {return val1 + val2;}); 2.1
+
             System.Console.ReadLine();
         }
 
@@ -24,6 +37,8 @@ namespace Workplace.Console
         //    System.Console.WriteLine($"OnOutBountDelegate perform {args.MathResult}");
         //} 
         // After we have added two services for notif. and logging we dont need this method anymore
+
+
 
     }
 }
